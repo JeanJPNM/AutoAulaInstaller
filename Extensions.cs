24f4b4a6aaa687268bbcc9684793b5e014440793
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-02-19 07:22:37 -0300

    program works

---
+++ Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Octokit;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Timers;
+namespace AutoAulaInstaller
+{
+    static class Extensions
+    {
+
+        public static ReleaseAsset? ProgramZip(this Release release)
+        {
+            var pattern = new Regex(@"release\.zip", RegexOptions.IgnoreCase);
+            var assets = new List<ReleaseAsset>(release.Assets);
+            return assets.Find((asset) => pattern.IsMatch(asset.Name));
+        }
+        /// <summary>
+        ///   Downloads the asset from github
+        /// </summary>
+        /// <param name="releaseAsset"></param>
+        /// <returns>The path to the downloaded file</returns>
+        public static async Task<string> Download(this ReleaseAsset releaseAsset)
+        {
+            using var webClient = new WebClient();
+            using var timer = new Timer(1000);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler((sender, e) =>
+            {
+                const string label = "Baixando Asset:";
+                var percentage = $"{e.ProgressPercentage}%";
+                Console.Write($"\r{label} {releaseAsset.Name} ({percentage})");
+            });
+            var data = await webClient.DownloadDataTaskAsync(new Uri(releaseAsset.BrowserDownloadUrl));
+            var filePath = Path.Join(Program.InstallationPath, releaseAsset.Name);
+            await File.WriteAllBytesAsync(filePath, data);
+            Console.WriteLine();
+            return filePath;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+using Octokit;
+using System.IO;
+using IWshRuntimeLibrary;
+using static System.Environment;
+namespace AutoAulaInstaller
+{
+    class Program
+    {
+        public static string InstallationPath
+        {
+            get
+            {
+                var folderPath = GetFolderPath(SpecialFolder.UserProfile);
+                return Path.Join(folderPath, "appdata/local/programs/auto_aula");
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("JeanJPNM"));
+            var release = await client.Repository.Release.GetLatest("JeanJPNM", "auto_aula.dart");
+            var zipAsset = release.ProgramZip();
+            if (zipAsset == null)
+            {
+                Console.WriteLine("O arquivo zip não foi encontrado.");
+                Console.WriteLine("Tente novamente mais tarde");
+                return;
+            }
+            Console.WriteLine("Baixando release...");
+            var filePath = await zipAsset.Download();
+            ZipFile.ExtractToDirectory(filePath, InstallationPath, true);
+            var executablePath = Path.Join(InstallationPath, "auto_aula.exe");
+            var shell = new WshShell();
+            IWshShortcut shortcut =(IWshShortcut) shell.CreateShortcut(Path.Join(GetFolderPath(SpecialFolder.StartMenu), "Auto Aula.lnk"));
+            shortcut.TargetPath = executablePath.Replace('/', '\\');
+            shortcut.Save();
+        }
+    }
+}
